be93fdeb8533789d7d472f587b43de9326a5b827
ganzmarins <marina.sukhanova@wellsterhealth.com>
2024-05-23 20:47:58 +0200

    added models

---
+++ prototype/backend/models/projectModel.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+const projectSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    createdBy: userSchema,
+    status: {
+        type: String,
+        enum: ["Design", "Testing", "Deployed", "Done"],
+        required: true
+    },
+    deadline: {
+        type: Date,
+        required: true
+    }, 
+    priority: {
+        type: String,
+        enum: ["Low", "Medium", "High"],
+        default: "Medium",
+        required: true
+    }, 
+    permissions: [{user: userSchema, 
+                   level: {
+                        type: String,
+                        enum: ["Owner", "Editor", "Reader"],
+                        required: true
+                   }
+                  }]
+});
+
+const Project = mongoose.model('Project', projectSchema);
+export default Project;
---
+++ prototype/backend/models/userModel.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+      },
+      lastName: {
+        type: String,
+        required: true
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+\@.+\..+/, 'Please fill a valid email address']
+      },
+      password: {
+        type: String,
+        required: true
+      },
+      role: {
+        type: String,
+        enum: ['Owner', 'Editor', 'Observer'],
+      },
+      photo: {
+        type: String,
+        required: false
+      }
+}, {collection: "users"},{timestamps: true});
+
+const User = mongoose.model('User', userSchema);
+export default User;
