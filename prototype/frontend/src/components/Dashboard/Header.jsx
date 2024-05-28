c37f56cfcc406f86916c7e1b1377297a1eac9b70
Ravi Tamang <ge27zer@mytum.de>
2024-05-28 22:35:45 +0200

    Dashboard UI changes.

--- prototype/frontend/src/components/Dashboard/Header.jsx
+++ prototype/frontend/src/components/Dashboard/Header.jsx
@@ -1,16 +1,16 @@
 import { useState } from "react";
-import { Container, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 
 
 const Header = (props) =>{
     const title = props.title;
     return(
-        <div>
-            <Row className="pt-5 px-3">
+        <Row>
+            <Col className="pt-5 px-3 pb-3">
                 <span style={{fontSize: 20, fontWeight: 'bold'}}>{title}</span>
-            </Row>
+            </Col>
             <hr/>
-        </div>
+        </Row>
     )
 }
 
