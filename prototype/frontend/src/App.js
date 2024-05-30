83b5b5362eee3a62cf315cbbd3e15667e9e3c888
meriemyac <yacmeriem@outlook.fr>
2024-05-31 01:11:57 +0200

    Added FR - Merged Ravi and Fen's code

--- prototype/frontend/src/App.js
+++ prototype/frontend/src/App.js
@@ -1,16 +1,27 @@
-import { BrowserRouter , Routes, Route} from 'react-router-dom';
-import Dashboard from './pages/Dashboard.jsx';
-import Settings from './pages/Settings.jsx';
+// App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import LandingPage from './pages/LandingPage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import Dashboard from './pages/Dashboard.jsx';
+import Requirements from './pages/Requirements.jsx';
+import NotFound from './components/ErrorPages/NotFound.jsx';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-          <Routes>
-            <Route path = "/*" element = {<Dashboard/>}/>
-          </Routes>
-      </BrowserRouter>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/dashboard/*" element={<Dashboard />} />
+          <Route path="/dashboard/requirements/:projectId/*" element={<Requirements />} />       
+          <Route path="/404" element={<NotFound />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
