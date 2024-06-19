bfe267e202228c4f38ee6e4c6fd32b10f36de3e2
Marina Sukhanova <m.sukhanova@tum.de>
2024-06-19 09:06:20 +0200

    working code week3

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,36 +1,5 @@
 // src/pages/LoginPage.js
 import React, { useState } from 'react';
-import {Container} from 'react-bootstrap';
-import LoginForm from '../components/Login/LoginForm';
-import RegisterModal from '../components/Login/RegisterModal';
-import '../styles/LoginPage.css';
-
-const LoginPage = () => {
-
-  const [show, setShow] = useState(false); // State to control the visibility of the registration modal
- 
-  const handleShowRegister = () => {
-    setShow(true);
-  };
-
-  const handleClose = () => {
-    setShow(false);
-  };
-
-  return (
-    <div className="login-page">
-      <Container className="login-container d-flex flex-column align-items-center justify-content-center">
-        <LoginForm onShowRegister={handleShowRegister} />
-        <RegisterModal show={show} handleClose={handleClose}/>
-      </Container>
-    </div>
-  );
-};
-
-export default LoginPage;
-
-// src/pages/LoginPage.js
-import React, { useState } from 'react';
 import { Form, Button, Container, Modal, Col, Row} from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import FileUploadButton from '../components/Login/FileUploadButton';
--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -1,31 +1,9 @@
-<<<<<<< HEAD
-import { Container, Col, Row, Button, Card, Accordion } from "react-bootstrap";
-import Header from "../components/Dashboard/Header";
-import SideBar from "../components/Dashboard/SideBar";
-=======
 import { Container, Accordion } from "react-bootstrap";
 import Header from "../components/Dashboard/Header";
->>>>>>> marina-week2
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import '../styles/style.css';
 import BreadCrumbRow from "../components/Dashboard/BreadCrumbRow";
-<<<<<<< HEAD
-
-const Settings = () => {
-
-  const [isSubscriptionOpen, setIsSubscriptionOpen] = useState(false);
-  const [isAppearanceOpen, setIsAppearanceOpen] = useState(false);
-  const [isProfileSettingsOpen, setIsProfileSettingsOpen] = useState(false);
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  useEffect(() => {
-    if (location.state?.openSubscription) {
-      setIsSubscriptionOpen(true);
-    }
-  }, [location.state]);
-=======
 import SubscriptionModal from "../components/Settings/SubscriptionModal";
 import SubscriptionPlanAccordion from '../components/Settings/SubscriptionPlanAccordion';
 import AppearanceAccordion from '../components/Settings/AppearanceAccordion';
@@ -59,32 +37,12 @@ const Settings = () => {
       setIsActiveKey(null)
     }
   }, []);
->>>>>>> marina-week2
 
   const goToPayment = () => {
     navigate('./payment');
   };
 
   const toggleSubscription = () => {
-<<<<<<< HEAD
-    setIsSubscriptionOpen(!isSubscriptionOpen);
-    setIsAppearanceOpen(false); // Close Appearances if it's open
-    setIsProfileSettingsOpen(false); // Close Profile Settings if it's open
-  };
-
-  const toggleAppearance = () => {
-    setIsAppearanceOpen(!isAppearanceOpen);
-    setIsSubscriptionOpen(false); // Close Subscription Plan if it's open
-    setIsProfileSettingsOpen(false); // Close Profile Settings if it's open
-  };
-
-  const toggleProfileSettings = () => {
-    setIsProfileSettingsOpen(!isProfileSettingsOpen);
-    setIsSubscriptionOpen(false); // Close Subscription Plan if it's open
-    setIsAppearanceOpen(false); // Close Appearances if it's open
-  };
-
-=======
     if (isSubscriptionOpen) {
     setIsActiveKey(null); // Set active key to null to close the accordion item entirely
   } else {
@@ -147,68 +105,10 @@ const Settings = () => {
   };
 
 
->>>>>>> marina-week2
   return (
     <Container fluid>
         <BreadCrumbRow/>
        <Header title="Settings Page" />
-<<<<<<< HEAD
-          <Accordion>
-            <Accordion.Item eventKey="0" className="accordion-item">
-              <Accordion.Header onClick={toggleSubscription} className="accordion-item-header">
-                  <Col xs={9}>
-                  <span className="settings-header">Subscription Plan</span>
-                  </Col>
-                  <Col xs={2}>
-                  <Button
-                    onClick={(e) => { e.stopPropagation(); goToPayment(); }}
-                    className="ms-auto"
-                    style={{ backgroundColor: "rgb(62,30,65)", color: 'white', border: 'none', borderRadius: '5px' }}> 
-                    Edit Payment Method
-                  </Button>
-                  </Col>
-              </Accordion.Header>
-              <Accordion.Body>
-                {isSubscriptionOpen && (
-                  <div className="content">
-                    {/* Display subscription plans as cards horizontally */}
-                    <div className="subscription-cards d-flex">
-                      <Card className="card mx-2">Plan 1</Card>
-                      <Card className="card mx-2">Plan 2</Card>
-                      <Card className="card mx-2">Plan 3</Card>
-                      <Card className="card mx-2">Plan 4</Card>
-                      <Card className="card mx-2">Plan 5</Card>
-                    </div>
-                  </div>
-                )}
-              </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="1" className="accordion-item">
-              <Accordion.Header onClick={toggleAppearance} className="accordion-item-header">
-              <span className="settings-header">Appearances</span>
-              </Accordion.Header>
-              <Accordion.Body>
-                {isAppearanceOpen && (
-                  <div className="content">
-                    Appearances content
-                  </div>
-                )}
-              </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="2" className="accordion-item">
-              <Accordion.Header onClick={toggleProfileSettings} className="accordion-item-header">
-              <span className="settings-header">Profile Settings</span>
-              </Accordion.Header>
-              <Accordion.Body>
-                {isProfileSettingsOpen && (
-                  <div className="content">
-                    User Profile Settings: Change Password, Profile Pic
-                  </div>
-                )}
-              </Accordion.Body>
-            </Accordion.Item>
-          </Accordion>
-=======
        <Accordion activeKey={isActiveKey}>
         <SubscriptionPlanAccordion
           toggleSubscription={toggleSubscription}
@@ -237,7 +137,6 @@ const Settings = () => {
               onHide={handleCloseModal}
               plan={selectedPlan}
             />
->>>>>>> marina-week2
     </Container>
   );
 };
