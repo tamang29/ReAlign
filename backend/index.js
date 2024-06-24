9dba64e33a093cffdd8343e643f3628ad56bddf3
Marina Sukhanova <m.sukhanova@tum.de>
2024-06-24 14:45:37 +0200

    port = 3000

--- backend/index.js
+++ backend/index.js
@@ -11,5 +11,5 @@ const app = express();
 app.use(express.json());
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
