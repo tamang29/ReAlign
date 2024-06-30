bbc72779347d6fba8c8afdb1164d116dc527593b
Ravi Tamang <ge27zer@mytum.de>
2024-06-30 19:18:14 +0200

    Added: Remodeling export feature

--- backend/routes/export.js
+++ backend/routes/export.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const router = express.Router();
-import {exportAsPDF} from '../controllers/exportController.js'
+import {convertSvgToPDF} from '../controllers/exportController.js'
 
-router.post('/file', exportAsPDF);
+router.post('/file', convertSvgToPDF);
 
 export default router;
